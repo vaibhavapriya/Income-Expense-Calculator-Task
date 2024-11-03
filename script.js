8b6b8cd9430d44d9af53cac46213ef47a4bb9a2b
vaibhavapriya <vaibhavapriyandv@gmail.com>
2024-11-03 23:59:19 +0530

    modified

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+//Add buttons
 const in_b=document.getElementById("income-button");
 const ex_b=document.getElementById("expense-button");
-//popups
+//popups class
 const popupi = document.getElementsByClassName("add-popi")[0];
 const popupe = document.getElementsByClassName("add-pope")[0];
+//submit buttons inform
+const in_s=document.getElementById("income-submit");
+const ex_s=document.getElementById("expense-submit");
+//list and items
+const itemList = document.getElementsByClassName("list")[0];
+const item=document.getElementById("item");
+list=[];
+var i=0;
 //on click for popup
 in_b.addEventListener("click",function(){
     popupi.classList.toggle("show");
@@ -10,10 +19,6 @@ in_b.addEventListener("click",function(){
 ex_b.addEventListener("click",function(){
     popupe.classList.toggle("show");
 });
-list=[];
-const in_s=document.getElementById("income-submit");
-const ex_s=document.getElementById("expense-submit");
-var i=0;
 in_s.addEventListener("click",function(){
     const t=document.getElementById("it");
     const n =document.getElementById("in");
@@ -22,14 +27,16 @@ in_s.addEventListener("click",function(){
         let obj={};
         obj.id=i;   
         obj.ie="income";
-        obj.type=t;
-        obj.name=n;
-        obj.amount=a;
+        obj.type=t.value.trim();;
+        obj.name=n.value.trim();;
+        obj.amount=a.value.trim();;
         list.push(obj);
         i++;
-        //add item to items
-        //change con- head
+        //add item to UI
+        addItem(obj,i);
+        //change con-head
     }
+    clearInputFieldsi();
     popupi.classList.toggle("show");
 });
 ex_s.addEventListener("click",function(){
@@ -37,23 +44,42 @@ ex_s.addEventListener("click",function(){
     const n=document.getElementById("en");
     const a=document.getElementById("ea");
     if(t&&n&&a){
-        let obj={id:i,ie:"expense",type:t,name:n,amount:a};
+        let obj={id:i,ie:"expense",type:t.value.trim(),name:n.value.trim(),amount:a.value.trim()};
         list.push(obj);   
         i++;
-        debugger;
-        //add item to items
+        //add item to UI       
+        addItem(obj,i);
         //change con-head
     }
+    clearInputFieldse();
     popupe.classList.toggle("show");
 });
-
-// add income
-// add expense
-//add popup
+//add popup FOR EDIT
 //edit i/e
 //change header display
-// list=[
-//     //each as income/expense,id,amount,type,describtion
-//    // {id: ,ie: ,type: ,name: ,amount:}
-
-// ];
+//filter
+//pie chart
+function addItem(object,index){
+    const newItem = item.cloneNode(true);
+    newItem.id = object.id;
+    newItem.classList.remove("hidden");
+    //newItem.querySelector(".btn").innerElement=newItem.createTextNode("edit");
+    newItem.querySelector(".ie").innerText=object.ie || "N/A";
+    newItem.querySelector(".type").innerText=object.type || "N/A";
+    newItem.querySelector(".name").innerText=object.name || "N/A";
+    newItem.querySelector(".amount").innerText=object.amount || "N/A";
+    itemList.appendChild(newItem);
+    debugger;
+    // var newdiv=document.createElement("div");
+    // newdiv.classList.add("items");
+};
+function clearInputFieldsi() {
+    document.getElementById("it").value = "";
+    document.getElementById("in").value = "";
+    document.getElementById("ia").value = "";
+}
+function clearInputFieldse() {
+    document.getElementById("et").value = "";
+    document.getElementById("en").value = "";
+    document.getElementById("ea").value = "";
+}
